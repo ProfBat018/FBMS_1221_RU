0b44b9a5888dbb209c1f6771fb1c135ec1c016ce
Elvin <elvin.azim01@gmail.com>
2022-06-27 10:54:24 +0400

    Cycles Repeat

---
+++ C++/Cycles/For/For/For.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// i - iteration 
+
+	/*for (size_t i = 0; i < 10; ++i)
+	{
+		cout << i << ' ';
+	}*/
+
+	//for (size_t i = 0, j = 10; i < j / 2 + i; i++, j--)
+	//{
+	//	cout << i << ' ' << j << '\n';
+	//}
+
+	/*for (size_t i = 0; i < 5; i++)
+	{
+		for (size_t j = i; j < 5; j++)
+		{
+			cout << '*' << ' ';
+		}
+		cout << endl;
+	}*/
+
+    return 0;
+}
