5590a234682522abbfed4fcdbcd8cb9e460554ed
Elvin <elvin.azim01@gmail.com>
2023-02-18 17:14:49 +0400

    Last updates

---
+++ Lesson2/Lesson2/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    public enum Gender
+    {
+        Female = 0,
+        Male = 1
+    };
+
+    public enum Role
+    {
+        Teacher = 0,
+        Student = 1
+    };
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+        public Role Role { get; set; }
+        public string Username { get; set; }
+        public SecureString Password { get; set; }
+    }
+}
