6df66b14a572ec12d0dd29fd6311ea25ab033b4d
Elvin <elvin.azim01@gmail.com>
2023-03-29 11:15:46 +0400

    Singleton and Prototype

---
+++ UML1/UML1/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace UML1;
+
+public class Person
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Name} {Surname} {Age}";
+    }
+}
