dfb4b93704f6edf50b98898e547f47f45538fab4
Elvin <elvin.azim01@gmail.com>
2023-03-31 09:34:07 +0400

    removed junk from patterns3

---
+++ Prototype/Prototype/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Diagnostics;
+
+var p1 = new Person()
+{
+    Name = "Elvin",
+    Surname = "Azimov",
+    Age = 21
+};
+
+// Shallow copy
+
+var p2 = p1.Clone() as Person;
+Console.WriteLine(p2.Name);
+p1.Name = "Samir";
+Console.WriteLine(p2.Name);
+
+
+
+class Person : ICloneable
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+    
+    public object Clone() => new Person()
+    {
+        Name = this.Name,
+        Surname = this.Surname,
+        Age = this.Age
+    };
+    
+}
+
+
