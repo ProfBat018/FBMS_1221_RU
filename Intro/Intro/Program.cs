0d1980be9764f68d6899975d5d6cbafb692ec17a
Elvin Azimov <elvin.azim01@gmail.com>
2023-10-18 11:43:18 +0400

    intro added

---
+++ Intro/Intro/Program.cs
@@ -0,0 +1,26 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages(); // Добавляю своему строителю поддержку Razor Pages
+
+
+// Доо этой строчки кода добавляются все настройки строителя
+var app = builder.Build(); // Создаю приложение
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error"); // Если приложение не в режиме разработки, то использую обработчик ошибок
+    app.UseHsts(); // Использую HSTS
+}
+
+// Все, что начинается с app.Use - это Middleware
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
